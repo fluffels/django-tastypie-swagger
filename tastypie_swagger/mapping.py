ea6d26bab16ac317b872288a4653f6364bdfd952
Jan CW Kroeze <thejcwk@gmail.com>
2014-09-17 11:44:48 +0200

    Apply patch from https://github.com/concentricsky/django-tastypie-swagger/issues/33 to enable PATCH endpoints.

--- tastypie_swagger/mapping.py
+++ tastypie_swagger/mapping.py
@@ -37,6 +37,7 @@ class ResourceSwaggerMapping(object):
         'post-list': "Create a new %s",
         'put-detail': "Update an existing %s",
         'delete-detail': "Delete an existing %s",
+        'patch-detail': "Partially update an existing %s",
     }
 
     def __init__(self, resource):
@@ -362,6 +363,11 @@ class ResourceSwaggerMapping(object):
             operation['parameters'].append(self.build_parameter_for_object(method='put'))
             detail_api['operations'].append(operation)
 
+        if 'patch' in self.schema['allowed_detail_http_methods']:
+            operation = self.build_detail_operation(method='patch')
+            operation['parameters'].append(self.build_parameter_for_object(method='patch'))
+            detail_api['operations'].append(operation)
+
         if 'delete' in self.schema['allowed_detail_http_methods']:
             detail_api['operations'].append(self.build_detail_operation(method='delete'))
 
@@ -423,8 +429,8 @@ class ResourceSwaggerMapping(object):
         properties = {}
 
         for name, field in self.schema['fields'].items():
-            # Exclude fields from custom put / post object definition
-            if method in ['post','put']:
+            # Exclude fields from custom put / post / patch object definition
+            if method in ['post','put','patch']:
                 if name in self.WRITE_ACTION_IGNORED_FIELDS:
                     continue
                 if field.get('readonly'):
@@ -541,6 +547,15 @@ class ResourceSwaggerMapping(object):
                     resource_name='%s_put' % self.resource._meta.resource_name,
                     properties=self.build_properties_from_fields(method='put'),
                     id='%s_put' % self.resource_name
+                )
+            )
+
+        if 'patch' in self.resource._meta.detail_allowed_methods:
+            models.update(
+                self.build_model(
+                    resource_name='%s_patch' % self.resource._meta.resource_name,
+                    properties=self.build_properties_from_fields(method='patch'),
+                    id='%s_patch' % self.resource_name
                 )
             )
 
